21572cae8ddea89628824d33e02581de76f73cad
devbekasyl <devbekasyl@gmail.com>
2026-06-19 12:31:11 +0500

    feat: user schema && prisma provider

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,12 +1,25 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Body } from '@nestjs/common';
 import { AppService } from './app.service.js';
+import { UserService } from './users.service.js';
+
+import { User as UserModel } from '../generated/prisma/client.js';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private readonly userService: UserService,
+  ) {}
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Post('user')
+  async signupUser(
+    @Body() userData: { name?: string; email: string; password: string },
+  ): Promise<UserModel> {
+    return this.userService.createUser(userData);
   }
 }
--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller.js';
 import { AppService } from './app.service.js';
+import { UserService } from './users.service.js';
+import { PrismaService } from './prisma.service.js';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, UserService, PrismaService],
 })
 export class AppModule {}
